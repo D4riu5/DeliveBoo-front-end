aa0886fd760e028b7fb5a18a72ee2093c1f90161
Brandon Vasquez <thatsjaskett@gmail.com>
2023-04-14 17:45:20 +0200

    Creato lo scheletro del sito

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,77 @@
+<script>
+import { store } from "../store.js";
+
+export default {
+    name: 'AppFooter',
+    components: {
+
+    },
+    data() {
+        return {
+            store
+        }
+    },
+    methods:{
+        
+    },
+}
+</script>
+
+<template>
+    <footer>
+        <div class="footer-container">
+            <div class="footer-section">
+                <h3>Info 1</h3>
+                <ul>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                </ul>
+            </div>
+            
+            <div class="footer-section">
+                <h3>Info 2</h3>
+                <ul>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                </ul>
+            </div>
+            
+            <div class="footer-section">
+                <h3>Info 3</h3>
+                <ul>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="footer-bottom">
+            <p>&copy; 2023 DeliveBoo - Tutti i diritti riservati.</p>
+        </div>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+    footer {
+        background-color: lightgray;
+        color: black;
+        padding: 40px 0;
+        border: 2px dashed black;
+    }
+    .footer-container {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+</style>
