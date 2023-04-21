bd0740e083621b8f59b5ceb676b7e2bd17637dad
Iacopo Buffoni <itsmadpupp@outlook.it>
2023-04-21 17:47:54 +0200

    Piccole modifiche header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -49,88 +49,52 @@ export default {
 </script>
 
 <template>
-    <header class="sticky-top p-4">
+    <header class="sticky-top p-2">
         <nav class="container d-flex justify-content-between">
-            <div class="logo-cont">
-                <a
-                    v-if="$route.path === '/'"
-                    class="text-decoration-none text-dark"
-                    href="#"
-                >
-                    <img src="" alt="Logo" />
-                    Nome Logo
+            <div class="logo">
+                <a v-if="$route.path === '/'" class="text-decoration-none text-dark" href="#">
+                    <img src="../img/1.png" alt="Logo" />
                 </a>
-                <router-link
-                    v-else
-                    class="text-decoration-none text-dark"
-                    :to="{
+                <router-link v-else class="text-decoration-none text-dark" :to="{
                         name: 'home',
-                    }"
-                >
+                    }">
                     <img src="" alt="Logo" />
                     Nome Logo
                 </router-link>
             </div>
 
-            <div>
+            <div class="d-flex align-items-center">
                 <!-- Button trigger modal -->
-                <button
-                    v-if="$route.path.includes('/Attivita')"
-                    type="button"
-                    class="btn btn-primary mx-2"
-                    data-bs-toggle="modal"
-                    data-bs-target="#exampleModal"
-                >
+                <button v-if="$route.path.includes('/Attivita')" type="button" class="btn btn-primary mx-2"
+                    data-bs-toggle="modal" data-bs-target="#exampleModal">
                     <i class="fa-solid fa-cart-shopping"></i>
                     <span class="badge bg-secondary ms-2">{{ cartCount }}</span>
                 </button>
 
                 <!-- Button trigger Offcanvas -->
-                <button
-                    class="btn btn-primary"
-                    type="button"
-                    data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasRight"
-                    aria-controls="offcanvasRight"
-                >
+                <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+                    aria-controls="offcanvasRight">
                     <i class="fa-solid fa-bars"></i>
                 </button>
             </div>
         </nav>
     </header>
     <!-- OFFCANVAS -->
-    <div
-        class="offcanvas offcanvas-end"
-        tabindex="-1"
-        id="offcanvasRight"
-        aria-labelledby="offcanvasRightLabel"
-    >
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="offcanvasRightLabel">MENU</h5>
-            <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-            ></button>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
             <div>
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a
-                            class="nav-link"
-                            aria-current="page"
-                            :href="this.store.backEndLink + '/login'"
-                            ><i class="fa-solid fa-user mx-2"> </i> Area Partner
+                        <a class="nav-link" aria-current="page" :href="this.store.backEndLink + '/login'"><i
+                                class="fa-solid fa-user mx-2"> </i> Area Partner
                         </a>
                     </li>
                     <li class="nav-item">
-                        <div
-                            v-if="darkTheme == false"
-                            @click="switchTheme()"
-                            id="changeTheme2"
-                        >
+                        <div v-if="darkTheme == false" @click="switchTheme()" id="changeTheme2">
                             <i class="fa-solid fa-sun fs-6 mx-2"></i> Light Mode
                         </div>
                         <div v-else id="changeTheme" @click="switchTheme()">
@@ -144,25 +108,14 @@ export default {
     <!-- OFFCANVAS -->
 
     <!-- SHOPPING CART MODAL -->
-    <div
-        class="modal fade modal-lg"
-        id="exampleModal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-    >
+    <div class="modal fade modal-lg" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="exampleModalLabel">
                         Shopping Cart
                     </h1>
-                    <button
-                        type="button"
-                        class="btn-close"
-                        data-bs-dismiss="modal"
-                        aria-label="Close"
-                    ></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <ul>
@@ -175,11 +128,7 @@ export default {
                     </ul>
                 </div>
                 <div class="modal-footer">
-                    <button
-                        type="button"
-                        class="btn btn-secondary"
-                        data-bs-dismiss="modal"
-                    >
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
                         Close
                     </button>
                     <button type="button" class="btn btn-primary">
@@ -208,8 +157,21 @@ header {
     #changeTheme {
         cursor: pointer;
     }
+
     #changeTheme2 {
         cursor: pointer;
+    }
+}
+
+.logo {
+    width: 130px;
+    height: 74px;
+    margin: 0px !important;
+
+    //DETTAGLI LOGO
+    img {
+        width: 100%;
+        height: 100%;
     }
 }
 </style>
