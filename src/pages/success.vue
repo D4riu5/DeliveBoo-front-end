472e6539647864064126ecef6ef4a73b59e055fe
D4riu5 <R3iji94@gmail.com>
2023-04-26 18:02:27 +0200

    changes: - added form to checkout page - removed test from router.js and pages - added a success page (redirected url after checkout)
    !no validation on checkout form yet
    !no dynamic total price in order yet

---
+++ src/pages/success.vue
@@ -0,0 +1,25 @@
+<script>
+// Helpers
+import { store } from "./../store";
+
+export default {
+    name: "success",
+    data() {
+        return {
+            store,
+        };
+    },
+    methods: {},
+};
+</script>
+
+<template>
+
+    <h1 class="m-5"> CIAO FROM SUCCESS</h1>
+
+
+</template>
+
+<style lang="scss">
+
+</style>
