a3b2c92ebbdf3268fe113b2d50950324e3378b5d
FeloMatta <mattafelo@gmail.com>
2023-05-01 17:37:04 +0200

    modificata pagina success

--- src/pages/success.vue
+++ src/pages/success.vue
@@ -15,11 +15,38 @@ export default {
 
 <template>
 
-    <h1 class="m-5"> CIAO FROM SUCCESS</h1>
-
+    <div class="container">
+        
+        <h3 class="my-bold">
+            Il tuo ordine è andato a buon fine!
+        </h3>
+        <p class="my-green my-bold">Controlla la tue email, per tutte le informazioni sul tuo ordine</p>
+        <p><strong class="my-bold">Grazie per aver scelto ComidaGo!</strong></p>
+        <router-link :to="{
+                        name: 'home',
+                    }">
+                    <button type="button" class="btn btn-danger">Home</button>
+        </router-link>
+    </div>
 
 </template>
 
 <style lang="scss">
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 50px;
+  background-color: #FFFFFF;
+  border-radius: 10px;
+  box-shadow: 0 0 20px #fc456a;
+
+    .my-green{
+        color: limegreen;
+    }
+    .my-bold{
+        font-weight: bold;
+    }
+}
+
 
 </style>
